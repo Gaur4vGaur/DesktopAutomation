a959c61cc5d2bc81adef5349267be81eeb946a27
gaur4vgaur <vaibgaurav@gmail.com>
2021-01-30 09:29:26 +0000

    Added changes to fetch publication database

---
+++ relseachpaper/PublicationDatabaseScrapper.py
@@ -0,0 +1,38 @@
+import selenium.webdriver.chrome.service as service
+from selenium import webdriver
+
+
+def wait_to_load_elements(elem):
+    text = elem.text
+    i = 0
+    while i < 50000 and text == "":
+        text = elem.text
+        # print(i)
+        i = i + 1
+
+    print("elements now available")
+
+
+def fetch_all_article_links(elem):
+    a_tags_in_element = elem.find_elements_by_tag_name("a")
+
+    tags = {a_tag.get_attribute('href') for a_tag in a_tags_in_element if
+            a_tag.get_attribute('href') and 'pubs' in a_tag.get_attribute('href')}
+    return tags
+
+
+if __name__ == "__main__":
+    service = service.Service('C:/Users/Admin/Downloads/softwares/chromedriver')
+    service.start()
+    driver = webdriver.Remote(service.service_url)
+
+    # print(date.today())
+
+    driver.get("https://research.google/pubs/?year=2021")
+    element = driver.find_element_by_class_name("search__cards")
+    wait_to_load_elements(element)
+    tags = fetch_all_article_links(element)
+    print(type(tags))
+    print(tags)
+
+    driver.quit()
