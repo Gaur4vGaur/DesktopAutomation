3201a07849dade32b35ac6a0aed4ca3c52960190
gaur4vgaur <vaibgaurav@gmail.com>
2021-01-30 09:51:34 +0000

    refactor code to create driver util

--- relseachpaper/PublicationDatabaseScrapper.py
+++ relseachpaper/PublicationDatabaseScrapper.py
@@ -1,17 +1,4 @@
-import selenium.webdriver.chrome.service as service
-from selenium import webdriver
-
-
-# The method is only used to wait and loop until elements are loaded
-def wait_to_load_elements(elem):
-    text = elem.text
-    i = 0
-    while i < 50000 and text == "":
-        text = elem.text
-        # print(i)
-        i = i + 1
-
-    print("elements now available")
+from util.BrowserUtil import driver, wait_to_load_elements
 
 
 def fetch_all_article_links(elem):
@@ -24,12 +11,6 @@ def fetch_all_article_links(elem):
 
 def publication_url(year):
     return f"https://research.google/pubs/?year={year}"
-
-
-def driver():
-    svc = service.Service('C:/Users/Admin/Downloads/softwares/chromedriver')
-    svc.start()
-    return webdriver.Remote(svc.service_url)
 
 
 if __name__ == "__main__":
