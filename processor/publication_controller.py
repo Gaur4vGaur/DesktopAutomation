76525f8469ac8683019a2fb37c0f4bd73a084078
Gaurav.Gaur <gaurav.gaur@ocado.com>
2021-02-01 16:54:07 +0000

    added dropdown to get the count

--- processor/publication_controller.py
+++ processor/publication_controller.py
@@ -1,8 +1,9 @@
 import datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 from publicationservice.publication_database_scrapper import publication_updates
+from publicationservice.publication_details import PublicationDetails
 
 app = Flask(__name__)
 
@@ -25,16 +26,34 @@ def loop():
 @app.route('/publications/')
 def publications():
     now = datetime.datetime.now()
-    update_count, record_count, pubs = publication_updates(now.year)
-    # update_count = 1
-    # pubs = [PublicationDetails(
-    #     "hero__title",
-    #     "hero__content",
-    #     "content__body",
-    #     "bar__title"
-    # )]
+    # update_count, record_count, pubs = publication_updates(now.year)
+    update_count = 1
+    record_count = 1
+    pubs = [PublicationDetails(
+        "hero__title",
+        "hero__content",
+        "content__body",
+        "bar__title",
+        "www.google.com"
+    )]
 
     return render_template('publications.html', updates=update_count, records=record_count, publications=pubs)
+
+
+@app.route('/publications/', methods=['post'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('read_count')  # access the data inside
+        print("form data")
+        print(username)
+
+        if username == 'root':
+            message = "Correct username and password"
+        else:
+            message = "Wrong username or password"
+
+    return redirect(url_for('publications'))
+
 
 #TODO
 # submit read count
